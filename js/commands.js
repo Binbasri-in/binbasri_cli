1fb606fb186072ee14b269c9ca8226d9ad3ae0e6
Mohammed Ali Alsakkaf (Binbasri) <mohammed.binbasri@gmail.com>
2024-03-11 19:44:54 +0530

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,101 @@
+var twitter = "https://twitter.com/md_ali_alsakkaf";
+var password = "binbasri";
+var linkedin = "https://www.linkedin.com/in/mohammed-ali-alsakkaf/";
+var github = "https://github.com/Binbasri-in";
+var email = 'mailto:mohammed.binbasri@gmail.com';
+var website = "https://binbasri.me/";
+var resume = "https://drive.google.com/file/d/1962BH6cZMzXQjTfl-s4X8_az7jDc8YiF/view?usp=sharing";
+
+var oss = ["", "https://github.com/Binbasri-in/translation-agency-system", "https://github.com/Binbasri-in/cold-calling-agent"]
+
+whois = [
+  "<br>",
+  "Hey, I'm Mohammed Ali!👋",
+  "I’m a software engineer From Yemen. Currently, I’m a Computer Science Engineering Student at BMSIT, Bengaluru, India.",
+  "My interest in computers started back when I was using the Internet and discovered the Open Source world .",
+  "Graudally, I started to learn more about computers and programming, but formally when I started CS50X course from Hravard.",
+  "To know more about me, you could enter <span class=\"command\">'projects'</span> to view my projects, <span class=\"command\">'social'</span> to view my social networks,",
+  "<span class=\"command\">'web'</span> for normal website or <span class=\"command\">'email'</span> to email me.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "vistor",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/binbasri' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/binbasri' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/binbasri' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Well... here's some Open Source projects!",
+  '<a href="'+oss[0]+'" target="_blank">GSoC RAG Chatbot</a>',
+  '<a href="'+oss[1]+'" target="_blank">Translation Agency System --in-progress</a>',
+  '<a href="'+oss[2]+'" target="_blank">Cold Calling AI</a>',
+  "(Spoiler: I'm working on many new projects, but they're not ready yet! So, stay tuned! (￣▽￣))",
+  "<br>"
+];
+
+ls = [
+    "<br>",
+    "I'm sorry but ls won't work here. (╯°□°）╯︵ ┻━┻",
+    "<br>"
+];
+
+cat = [
+    "<br>",
+    "I'm sorry but cat won't work here. (╯°□°）╯︵ ┻━┻",
+    "<br>"
+];
+
+beep = [
+    "<br>",
+    "boop",
+    "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who am I? ::>_<::',
+  '<span class="command">whoami</span>         Who are you? (╯▽╰ )',
+  '<span class="command">social</span>         Display social networks (´｡• ᵕ •｡`)',
+  '<span class="command">projects</span>       View projects (^人^)',
+  '<span class="command">history</span>        View command history (。_。)',
+  '<span class="command">help</span>           This MENU! (´｡• ᵕ •｡`)',
+  '<span class="command">email</span>          Do not email me (╯°□°）╯︵ ┻━┻',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header, again (°ー°〃)',
+  '<span class="command">secret</span>         Display secret commands (¬‿¬ )',
+  '<span class="command">web</span>            Display traditional website (￣▽￣)ノ',
+  '<span class="command">resume</span>         Display my resume (´｡• ᵕ •｡`)',
+  "Some linux commands are not supported here. I'm sorry! ＞︿＜",
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Initiating...</span>',
+    "   _        __  __      ____        U  ___ u", 
+    'U /"\\  u    \\ \\/"/   U |  _"\\ u      \\/"_ \\/', 
+    " \\/ _ \\/     /\\  /\\    \\| |_) |/      | | | | ",
+    "/ ___ \\    U /  \\ u    |  _ <    .-,_| |_| | ",
+    "/_/  \\_\\    /_/\\_\\     |_| \\_\\    \\_)-\\___/  ",
+    "\\\\    >>  ,-,>> \\\\_    //   \\\\_        \\\\    ",
+    "(__) (__)  \\_)  (__)  (__)  (__)      (__)   ",
+    "(c) 2024",
+    "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+  '<span class="color2">Welcome to my web terminal \\(￣︶￣*\\)).</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
